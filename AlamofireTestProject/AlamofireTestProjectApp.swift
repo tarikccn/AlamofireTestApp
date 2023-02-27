32407a6ab8507f7c5bdb4549fca27640032a9fa2
tarikccn <tarikcucun61@gmail.com>
2023-02-27 05:53:18 -0800

    Initial Commit

---
+++ AlamofireTestProject/AlamofireTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireTestProjectApp.swift
+//  AlamofireTestProject
+//
+//  Created by Bhop on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct AlamofireTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
